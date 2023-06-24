5b65ee7e779ba86373109c1b6edf7cef946bbb20
Fabiano Vilela <fabiano.vilela@gmail.com>
2023-06-24 11:29:59 -0300

    Login screen

--- App.tsx
+++ App.tsx
@@ -1,50 +1,12 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
-
-import {Text} from './src/components/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme';
-import {Button} from './src/components/Button';
-import {Box} from './src/components/Box';
-import {TextInput} from './src/components/TextInput';
-import {Icon} from './src/components/Icon';
+import {Login} from './src/assets/screens/auth/Login';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text marginBottom="s8" preset="headingLarge">
-            Olá
-          </Text>
-          <Text preset="paragraphLarge" mb="s40">
-            Digite seu e-mail e senha para entrar
-          </Text>
-
-          <Box mb="s20">
-            <TextInput
-              placeholder="Digite seu e-mail"
-              label={'E-mail'}
-              boxProps={{mb: 's20'}}
-            />
-          </Box>
-          <Box>
-            <TextInput
-              placeholder="Digite sua senha"
-              label={'Senha'}
-              RightComponent={<Icon name="eyeOn" color="gray2" />}
-              boxProps={{mb: 's10'}}
-            />
-          </Box>
-
-          <Text color="primary" preset="paragraphSmall" bold mt="s10">
-            Esqueci minha senha
-          </Text>
-
-          <Button marginTop="s48" title="Entrar" />
-          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
-        </View>
-      </SafeAreaView>
+      <Login />
     </ThemeProvider>
   );
 }
---
+++ src/assets/screens/auth/Login/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Text} from '../../../../components/Text';
+import {View, SafeAreaView} from 'react-native';
+import {TextInput} from '../../../../components/TextInput';
+import {Box} from '../../../../components/Box';
+import {Icon} from '../../../../components/Icon';
+import {Button} from '../../../../components/Button';
+
+export function Login() {
+  return (
+    <SafeAreaView>
+      <View style={{paddingHorizontal: 24}}>
+        <Text marginBottom="s8" preset="headingLarge">
+          Olá
+        </Text>
+        <Text preset="paragraphLarge" mb="s40">
+          Digite seu e-mail e senha para entrar
+        </Text>
+
+        <Box mb="s20">
+          <TextInput
+            placeholder="Digite seu e-mail"
+            label={'E-mail'}
+            boxProps={{mb: 's20'}}
+          />
+        </Box>
+        <Box>
+          <TextInput
+            placeholder="Digite sua senha"
+            label={'Senha'}
+            RightComponent={<Icon name="eyeOn" color="gray2" />}
+            boxProps={{mb: 's10'}}
+          />
+        </Box>
+
+        <Text color="primary" preset="paragraphSmall" bold mt="s10">
+          Esqueci minha senha
+        </Text>
+
+        <Button marginTop="s48" title="Entrar" />
+        <Button preset="outline" marginTop="s12" title="Criar uma conta" />
+      </View>
+    </SafeAreaView>
+  );
+}
