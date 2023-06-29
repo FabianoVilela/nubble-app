7fa2bc3a4e309d2a5c6aea76a089f6c2511dba76
Fabiano Vilela <fabiano.vilela@gmail.com>
2023-06-28 21:29:31 -0300

    SignIn and SignUp screens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme';
-import {Login} from './src/assets/screens/auth/Login';
+import {Login} from './src/screens/auth/Login';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SignUp} from './src/screens/auth/SignUp';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <Login />
+        {/* <Login /> */}
+        <SignUp />
       </ThemeProvider>
     </SafeAreaProvider>
   );
